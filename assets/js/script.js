bc01c11c0511ac2587c7650abef372993758296b
kenzinielsen <kenzinielsen@kenzis-air.lan>
2021-09-05 15:22:20 -0600

    searched city function

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,17 +5,40 @@ var currentTemp = document.querySelector(".currentTemp");
 var currentWind = document.querySelector(".currentWind");
 var currentHumidity = document.querySelector(".currentHumidity");
 var currentUV = document.querySelector(".currentUV");
+var searchResult = document.querySelector(".searchResult");
+
 
 function search(){
     fetch('https://api.openweathermap.org/data/2.5/weather?q=' +inputValue.value+ '&appid=f7099a6b828983b5d3dfefa48057b726')
     .then(function(response) {
-        return response.json();
-    })
-    .then(function(data) {
-        console.log(data);
-    })
+        response.json().then(function(data)  {
+             var cityResultEl = document.querySelector(".cityResult");
+             cityResultEl.innerHTML = data.name + "(" + moment(data.dt, "X").format("MM/DD/YYYY") + ") <img src=' http://openweathermap.org/img/wn/" + data.weather[0].icon +".png' >"
+        
+             var currentTempEl = document.querySelector(".currentTemp");
+             $(currentTempEl).empty().append("Temperature: " + data.main.temp + "°F");
 
-    //variable that will select where the info will be displayed
-    var searchResult = document.querySelector(".searchResult");
+             var currentWindEl = document.querySelector(".currentWind");
+             $(currentWindEl).empty().append("Wind: " + data.wind.speed);
+
+             var currentHumidityEl = document.querySelector(".currentHumidity");
+             $(currentHumidityEl).empty().append("Humidity: " + data.main.humidity);
+        
+        
+        })
+    })
 };
+
+//5 day forecast
+function forecast() {
+    fetch("https://api.openweathermap.org/data/2.5/forecast?q=" +inputValue.value + "&appid=f7099a6b828983b5d3dfefa48057b726")
+
+    
+    
+
+
+}
+
 searchBtn.addEventListener("click", search);
+
+//my api     f7099a6b828983b5d3dfefa48057b726

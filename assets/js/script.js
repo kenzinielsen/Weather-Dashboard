bc6ea1a5a4684553e31feaf4b1eac7bf34aa63f9
kenzinielsen <kenzinielsen@kenzis-air.lan>
2021-09-05 08:33:17 -0600

    fixed searched city button function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,21 @@
 var searchBtn = document.querySelector(".searchBtn");
-var inputValue = document.querySelector("#inputValue")
+var inputValue = document.querySelector(".inputValue");
 var name = document.querySelector(".name");
 var currentTemp = document.querySelector(".currentTemp");
 var currentWind = document.querySelector(".currentWind");
 var currentHumidity = document.querySelector(".currentHumidity");
 var currentUV = document.querySelector(".currentUV");
 
-searchBtn.addEventListener("click", function() {
-    fetch('http://api.openweathermap.org/data/2.5/weather?q='+inputValue+'&appid=f7099a6b828983b5d3dfefa48057b726')
+function search(){
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=' +inputValue.value+ '&appid=f7099a6b828983b5d3dfefa48057b726')
     .then(function(response) {
         return response.json();
     })
     .then(function(data) {
         console.log(data);
-    });
+    })
 
-})
-
-
+    //variable that will select where the info will be displayed
+    var searchResult = document.querySelector(".searchResult");
+};
+searchBtn.addEventListener("click", search);
